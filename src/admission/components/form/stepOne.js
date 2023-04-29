1e0a739e67e941eb28b434cf5983f4ede71f53a8
edgaratamali <edgar.mutangana@amalitech.com>
2023-04-29 15:02:23 +0200

    sale3

--- src/admission/components/form/stepOne.js
+++ src/admission/components/form/stepOne.js
@@ -9,37 +9,44 @@ function StepOne() {
      {/* <p>Please provide your name, email address, and phone number.</p> */}
    </div>
    <InputField
+      id="info_student_names_stepOne"
       label="Student Names"
       type="text"
       placeholder="e.g. Mugabo Greg"
      />
      <InputField
+      id="info_student_previous_year_of_study_stepOne"
       label="Student Previous Year of Study"
       type="text"
       placeholder="e.g. P3 or Primary 3 or Primary Three"
      />
      <InputField
-      label="Student Current Year of Study"
+     id="info_student_current_year_of_study_stepOne"
+      label="Student Current or Next Year of Study"
       type="text"
       placeholder="e.g. P4 or Primary 4 or Primary Four"
      />
      <InputField
+     id="info_previous_school_stepOne"
       label="Which School Did The Student Go To?"
       type="text"
       placeholder="e.g. X - Primary School or X - Nursery School"
      />
 
 <InputField
+  id="info_dob_stepOne"
       label="Student Date of Birth"
       type="date"
       placeholder=""
      />
      <InputField
+     id="info_pob_stepOne"
       label="Student Place of Birth"
       type="text"
       placeholder="e.g. Kacyiru, Kigali - Rwanda"
      />
      <InputField
+     id="info_sa_stepOne"
       label="Student Address"
       type="text"
       placeholder="e.g. Kimironko, Gasabo"
--- src/donation/donation.js
+++ src/donation/donation.js
@@ -2,13 +2,15 @@ import React from "react";
 import Footer from "./components/footer/footer";
 import NavBar from "./components/navbar/navbar";
 
-import "./donation.css"
+import "./Donation.css"
+import FormAdmissions from "./components/form/form";
 
 function Donation() {
     return (
         <>
             <NavBar />
-                <h1 style={{margin: "150px auto", width: "fit-content", height: "50px", fontFamily: "gilli"}}>Page Under Construction</h1>
+            <FormAdmissions />
+            {/* <h1 style={{margin: "150px auto", width: "fit-content", height: "50px", fontFamily: "gilli"}}>Page Under Construction</h1> */}
             <Footer />
         </>
     )
